0c061241380723b5f73f765c26fb647ecd41efd3
Aleksandr <aleksandr.krylov@decathlon.com>
2020-06-13 02:24:12 +0300

    fix

--- src/components/TasksPace.vue
+++ src/components/TasksPace.vue
@@ -20,7 +20,7 @@
                 <div v-if="(index + 1) == show" class="mini-block-with-tasks">
                   <div class="one-task" v-for="(itm, ind) in item.list" :key="ind">
                     <span>{{itm.task}}</span>
-                    <div class="tasks-points">
+                    <div class="tasks-points" v-if="itm.points.length">
                       <div
                         class="tasks-points-box"
                         :style="{
